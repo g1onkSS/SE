4a7832a42a62650154f10acc7817d6f39a976f31
Artem Varlakov <kkg27466@gmail.com>
2024-12-20 15:59:37 +0500

    Третий коммит

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
-using namespace std;
-
 int main()
 {
-
+	int a, b;
+	std::cout << "Enter A and B: ";
+	std::cin >> a >> b;
+	std::cout << "A = " << a << '\n';
+	std::cout << "B = " << b << '\n';
+	return 0;
 }
