ed5487c1ab00746ad3b13eb444bdf26bc0ce8852
Artem Varlakov <kkg27466@gmail.com>
2024-12-20 16:24:46 +0500

    Шестой коммит добавление разности чисел

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 int main()
 {
-	
+	int a, b;
+	std::cout << "Enter A and B: ";
+	std::cin >> a >> b;
+	std::cout << "A = " << a << '\n';
+	std::cout << "B = " << b << '\n';
+	std::cout << "A + B = " << a + b << '\n';
+	std::cout << "A - B = " << a - b << '\n';
+	return 0;
 }
